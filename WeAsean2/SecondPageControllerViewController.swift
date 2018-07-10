70aecb038a8996ce1601965c67663d6810052fff
Hamdy <hamdy.ali@sphiere.com.bn>
2018-07-10 16:42:27 +0800

    Implemented 2 pages with storyboard and can navigate between the 2

---
+++ WeAsean2/SecondPageControllerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPageControllerViewController.swift
+//  WeAsean2
+//
+//  Created by SPHIERE on 7/10/18.
+//  Copyright © 2018 SPHIERE. All rights reserved.
+//
+
+import UIKit
+
+class SecondPageControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
